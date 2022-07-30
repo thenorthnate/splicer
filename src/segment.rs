afa0a3848c32ef5dedda52a7e30a78522465e0bc
Nathan North <nathantnorth@gmail.com>
2022-07-30 10:21:03 -0700

    moving some definitions around

--- src/segment.rs
+++ src/segment.rs
@@ -2,60 +2,9 @@
 //!
 //! Defines any contiguous segment of nucleotides
 
-#[derive(Debug, PartialEq)]
-pub enum Strand {
-    Forward,
-    Reverse,
-}
 
-#[derive(Debug, PartialEq)]
-pub enum SegmentType {
-    CDS,
-    Exon,
-    ExonFivePrime,
-    ExonThreePrime,
-    Gene,
-    Intron,
-    StartCodon,
-    Transcript,
-    Unknown,
-}
 
-#[derive(Debug)]
-pub struct Segment {
-    pub index: Option<u32>,
-    pub id: Option<String>,
-    pub segment_type: SegmentType,
-    /// start - Start position* of the feature, with sequence numbering starting at 1.
-    pub start: u32,
-    /// end - End position* of the feature, with sequence numbering starting at 1.
-    pub end: u32,
-    pub version: Option<u32>,
-    pub score: Option<f32>,
-}
 
-impl Segment {
-    pub fn new(start: u32, end: u32, segment_type: SegmentType) -> Segment {
-        Segment{
-            index: None,
-            id: None,
-            segment_type: segment_type,
-            start: start,
-            end: end,
-            version: None,
-            score: None,
-        }
-    }
-}
-
-pub struct Gene {
-    id: String,
-    version: u32,
-    name: String,
-    source: String,
-    gene_biotype: String,
-    segment: Segment,
-}
 
 
 // /// An entry in a GTF or GFF-Version 2 file: https://uswest.ensembl.org/info/website/upload/gff.html
